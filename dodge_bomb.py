bf32abc24173406bace886173770c9dcc9eeddf7
c0a24043b5 <c0a24043b5@edu.teu.ac.jp>
2025-09-30 14:38:33 +0900

    画面の外に行かないやつ

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -15,6 +15,20 @@ DELTA = {
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
+def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    引数:こうかとんRact or 爆弾Ract
+    戻り値:判定結果タプル(横縦方向)
+    画面内:True/画面外:False
+    """
+    yoko, tate = True, True
+    if rct.left < 0 or WIDTH < rct.right: # 横方向のはみ出しチェック
+        yoko = False
+    if rct.top < 0 or HEIGHT < rct.bottom: # 縦方向のはみ出しチェック
+        tate = False
+    return yoko, tate
+
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -52,8 +66,15 @@ def main():
         # if key_lst[pg.K_RIGHT]:
         #     sum_mv[0] += 5
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
         bb_rct.move_ip(vx, vy)
+        yoko, tate = check_bound(bb_rct)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1 
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
